981dace7ced5fe6eb13f0ce60d5c9516fa3a71ad
LennyRemache <lenny.j.remache@gmail.com>
2022-08-14 20:39:24 -0400

    Creating custom components

--- info-site/index.js
+++ info-site/index.js
@@ -1,15 +1,17 @@
-const page = (
-  <div>
-    <img src="./react-logo.png" width="100px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Power thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function Page() {
+  return (
+    <div>
+      <img src="./react-logo.png" width="100px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
