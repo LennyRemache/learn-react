3bb404b33c78ca9f11f66243257e291a17e2eba5
LennyRemache <lenny.j.remache@gmail.com>
2022-08-14 23:44:08 -0400

    Added and styled  Navbar component

--- info-site/src/components/Navbar.js
+++ info-site/src/components/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ReactLogo from "../images/react-logo.png";
+import "../style.css";
 
 function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <>
+      <nav className="nav-bar">
+        <div className="main-logo">
+          <img src={ReactLogo} width="40px" alt="React.js Logo" />
+          <h2>ReactFacts</h2>
+        </div>
+        <h3 className="course-name">React Course - Project 1</h3>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
