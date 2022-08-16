26bd654cb24fff56ac1d7c7da26a8e8d1e5d557a
LennyRemache <lenny.j.remache@gmail.com>
2022-08-16 16:20:43 -0400

    Rendering multiple components using the array map() method and reacts built in ability to render each array element

---
+++ contact-card/src/catData.js
@@ -0,0 +1,33 @@
+import felix from "./images/felix.jpeg";
+import fluffykins from "./images/fluffykins.jpeg";
+import pumpkin from "./images/pumpkin.jpeg";
+import whiskerson from "./images/whiskerson.jpeg";
+
+const catData = [
+  {
+    img: felix,
+    name: "Felix",
+    phone: "(212) 555-4567",
+    email: "thecat@hotmail.com",
+  },
+  {
+    img: fluffykins,
+    name: "Fluffykins",
+    phone: "(212) 555-2345",
+    email: "fluff@me.com",
+  },
+  {
+    img: pumpkin,
+    name: "Pumpkin",
+    phone: "(0800) CAT KING",
+    email: "pumpkin@scrimba.com",
+  },
+  {
+    img: whiskerson,
+    name: "Mr. Whiskerson",
+    phone: "(212) 555-1234",
+    email: "mr.whiskaz@catnap.meow",
+  },
+];
+
+export default catData;
