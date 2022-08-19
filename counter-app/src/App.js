48a43c34b73866bb7ca71aaad34156757ad31259
LennyRemache <lenny.j.remache@gmail.com>
2022-08-18 20:02:08 -0400

    Complex state with Objects

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -15,8 +15,23 @@ function App() {
     return <p>{thing}</p>;
   });
 
+  const [personObj, setPersonObj] = React.useState({
+    first_name: "Lenny",
+    last_name: "Remache",
+    isHere: false,
+  });
+
+  function isHere() {
+    setPersonObj((prevPersonObj) => ({
+      ...prevPersonObj,
+      isHere: !prevPersonObj.isHere,
+    }));
+  }
+
   return (
     <div className="App">
+      <button onClick={isHere}>Is here?</button>
+      <p>{personObj.isHere ? "Yes" : "No"}</p>
       <button onClick={addThing}>Add Thing</button>
       {renderThingsArr}
     </div>
