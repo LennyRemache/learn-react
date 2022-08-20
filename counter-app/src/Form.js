825169929fba6f71d3177554cd95fb9f599ce30c
LennyRemache <lenny.j.remache@gmail.com>
2022-08-20 15:03:23 -0400

    Computed properties

--- counter-app/src/Form.js
+++ counter-app/src/Form.js
@@ -1,17 +1,45 @@
 import React from "react";
 
 export default function Form() {
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  console.log(formData);
+
   function handleChange(event) {
-    console.log(event.target.value);
-    setFirstName(event.target.value);
-    setLastName(event.target.value);
+    // event.target is a DOM element where handleChange is used
+    setFormData((prevForm) => {
+      return {
+        ...prevForm,
+        // computed properties -> [<variable with name stored in it>], otherwise without the brackets itll be a syntax error
+        [event.target.name]: event.target.value,
+      };
+    });
   }
+
   return (
     <form>
-      <input type="text" placeholder="First Name" onChange={handleChange} />
-      <input type="text" placeholder="Last Name" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+      />
     </form>
   );
 }
