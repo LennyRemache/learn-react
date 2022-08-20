f7d59aa0e66e91caaca2eb4fe4bb069eb142ef9c
LennyRemache <lenny.j.remache@gmail.com>
2022-08-20 15:35:46 -0400

    React Form checkboxes

--- counter-app/src/Form.js
+++ counter-app/src/Form.js
@@ -5,17 +5,20 @@ export default function Form() {
     firstName: "",
     lastName: "",
     email: "",
+    isFriendly: true,
   });
 
   console.log(formData);
 
   function handleChange(event) {
     // event.target is a DOM element where handleChange is used
+    const { name, value, type, checked } = event.target;
     setFormData((prevForm) => {
       return {
         ...prevForm,
         // computed properties -> [<variable with name stored in it>], otherwise without the brackets itll be a syntax error
-        [event.target.name]: event.target.value,
+        //[event.target.name]: event.target.value,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
   }
@@ -27,19 +30,33 @@ export default function Form() {
         placeholder="First Name"
         onChange={handleChange}
         name="firstName"
+        value={formData.firstName}
       />
       <input
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
         name="lastName"
+        value={formData.lastName} // allows for state of value to be controlled by react rather than HTML's input tag
       />
       <input
         type="email"
         placeholder="Email"
         onChange={handleChange}
         name="email"
+        value={formData.email}
       />
+
+      {/* <textarea value={}/> -> textarea is self closing in react */}
+
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
     </form>
   );
 }
